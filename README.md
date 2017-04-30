5c272cd43bd908b36e9c881c3130fa537fc855d7
fernandobd42 <fernandolucasbd@gmail.com>
2017-04-30 17:26:19 +0000

    boilerplate sass configured, now just use

---
+++ README.md
@@ -0,0 +1,7 @@
+# sass
+
+### initiate
+just run: yarn
+
+### to build
+just run: yarn start
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
